7bbf1f93879e1de592fb8af4c398fdb4d91330cb
Josh B <josh.ja.butt@gmail.com>
2020-04-14 21:33:51 +1000

    Setup basic shell app

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from aiohttp import web
+import aiohttp
 import aiohttp_jinja2
+import jinja2
+
+from aiohttp import web
 
 class Server(web.Application):
 
@@ -7,10 +10,17 @@ class Server(web.Application):
         super().__init__()
 
         self.add_routes([
-            web.static('/static', static),
+            web.static('/static', 'static'),
             web.get('/', self.index)
         ])
 
     @aiohttp_jinja2.template('core.jinja2')
     async def index(self, request: web.Request):
         return {}
+
+if __name__ == "__main__":
+    app = Server()
+    
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
+
+    web.run_app(app, host='localhost', port=8081)
